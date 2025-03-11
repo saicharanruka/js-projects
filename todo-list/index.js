9622722ee39cb64d396013aa7cf3662de5f6cefd
slayer0x10 <saicharanruka@gmail.com>
2025-03-11 17:02:57 +0530

    finished todo list

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,9 +1,11 @@
 import express from "express"
+import bodyParser from "body-parser"
 
 const app = express()
 const PORT = 3000
 var tasks = []
 
+app.use(bodyParser.urlencoded({extended : true}))
 app.use(express.static("public"))
 
 app.get("/", (req,res)=> {
@@ -14,12 +16,23 @@ app.get("/", (req,res)=> {
 })
 
 app.post("/submit", (req,res)=> {
-    
+    tasks.push(req.body.task)
+    res.redirect("/")
+
+})
+
+app.get("/delete", (req,res) => {
+    var remove  = req.query.id
+    var index = tasks.indexOf(remove)
+    tasks.splice(index,1)
+    res.redirect("/")
+
 })
 
 
 app.listen(PORT, (req,res)=> {
-    console.log("Listening on port ", PORT)
+    console.log("Listening on port", PORT)
 })
+
 
 
